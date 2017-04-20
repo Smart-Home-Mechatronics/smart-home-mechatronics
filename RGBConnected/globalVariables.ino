5fa5be1f6898fa4a84d3260a876deb3a0b751aaa
Smart-Home-Mechatronics <ihome.webserver@gmail.com>
2017-04-20 10:25:25 -0400

    Add files via upload

---
+++ RGBConnected/globalVariables.ino
@@ -0,0 +1,10 @@
+int notificationStatus = 0; 
+//0: Normal
+//1: Romantic
+//2: Alert
+//3: EmergencyAlert
+
+
+int RPin = D0;      // RED LED connected to digital pin D0
+int GPin = D1;      // GREEN LED connected to digital pin D1
+int BPin = D2;      // BLUE LED connected to digital pin D2
